a8d53cd4fd0e1c659c7b9d2422461a784a9874af
Michael Bestas <mkbestas@lineageos.org>
2018-02-27 20:00:45 +0000

    RMX1801: Set recovery fstab
     * Mounting /system and /vendor partitions is handled in kernel now,
       however removing the entries from fstab caused issues building
       the OTA. The workaround was to have a separate fstab, but turns out
       that simply setting the recoveryonly flag does the trick because
       those are then ignored during a normal Android boot.
    
    Change-Id: I7243c0f3590998e81057b8416640696b5183bfb2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,9 @@ TARGET_BOARD_PLATFORM := sdm660
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
 # SEPolicy
 #include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
