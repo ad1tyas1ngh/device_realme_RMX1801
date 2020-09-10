4c5081d885e52fa194ed2f06330f39130cfadeab
luk1337 <priv.luk@gmail.com>
2020-09-10 03:57:15 +0200

    RMX1801: Build libhidltransport and libhwbinder
    Change-Id: Icf8b8dda7097572106bd948e4a51cf5663ed781d

--- device.mk
+++ device.mk
@@ -146,6 +146,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/irq/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
