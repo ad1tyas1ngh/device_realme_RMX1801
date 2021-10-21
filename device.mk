9d006fbed83a288c4ec7ef1730eb711f2baba7b3
Debayan Kar <kardebayan3@gmail.com>
2021-10-21 11:24:12 +0530

    RMX1801: Import rootdir files from stock
    * From sdm660_64-user 10 QKQ1.191014.001 eng.root.20201016.161857 release-keys
    
    Change-Id: I5772b48a9cc38ff3532513767f64e1315c5c23e9
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

---
+++ device.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    engineer_vendor_shell.sh \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.oppo.face.sh \
+    init.oppo.face_calib.sh \
+    init.oppo.fingerprints.sh \
+    init.oppo.wifi.sh \
+    init.oppo.wifiAutoRecovery.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.can.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    init.qti.qseecomd.sh \
+    oppo-install-recovery.sh \
+    qca6234-service.sh \
+    vendor.autochmod.sh \
+    init.msm.usb.configfs.rc \
+    init.oppo.debug.diag.rc \
+    init.oppo.reserve.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    fstab.qcom \
+    ueventd.qcom.rc
