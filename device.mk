8d7347b587d449c7b6df77a6ffbe8e01b39bebc2
Debayan Kar <kardebayan3@gmail.com>
2021-10-21 15:20:55 +0530

    RMX1801: Import excluded-input-devices.xml from stock
    * From sdm660_64-user 10 QKQ1.191014.001 eng.root.20201016.161857 release-keys
    
    Change-Id: I66e23570cf996756fc2a1a82c69bca0de4184a29
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -133,6 +133,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/excluded-input-devices.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/excluded-input-devices.xml \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # WiFi
